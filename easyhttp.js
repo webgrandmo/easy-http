447325f6de57a5858fb7690ada1b88e12a7a8e27
andy <webgrandmo@gmail.com>
2019-05-05 22:45:26 +0300

    feature/Create-http-methods. Add GET, POST, PUT and DELETE methods

---
+++ easyhttp.js
@@ -0,0 +1,73 @@
+function easyHttp() {
+  this.http = new XMLHttpRequest();
+}
+
+//Make an HTTP GET request
+
+easyHttp.prototype.get = function (url, callback) {
+  this.http.open('GET', url, true);
+
+  let self = this;
+
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+
+      callback(null, self.http.responseText);
+    } else {
+      callback(`Error: ${self.http.status}`);
+    }
+  }
+
+  this.http.send();
+}
+
+//Make an HTTP POST request
+
+easyHttp.prototype.post = function (url, data, callback) {
+  this.http.open('POST', url, true);
+
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function () {
+
+    callback(null, self.http.responseText);
+  }
+
+  this.http.send(JSON.stringify(data));
+}
+
+//Make an HTTP PUT request
+
+easyHttp.prototype.put = function (url, data, callback) {
+  this.http.open('PUT', url, true);
+
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function () {
+
+    callback(null, self.http.responseText);
+  }
+
+  this.http.send(JSON.stringify(data));
+}
+
+//Make an HTTP DELETE request
+
+easyHttp.prototype.delete = function (url, callback) {
+  this.http.open('DELETE', url, true);
+
+  let self = this;
+
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+
+      callback(null, 'Post deleted');
+    } else {
+      callback(`Error: ${self.http.status}`);
+    }
+  }
+
+  this.http.send();
+}
